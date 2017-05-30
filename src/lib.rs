fe0b497717d3d79d83a6e620bad6d6bdfb241555
Geordon Worley <vadixidav@gmail.com>
2017-05-30 09:33:26 -0400

    add more basic struct elements

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,29 @@
+#![feature(inclusive_range)]
+
+use std::ops::RangeInclusive;
+
+struct BusState {
+    /// Bus handler address
+    handler: Vec<u32>,
+    /// Bus interrupt enable
+    enabled: bool,
+    /// Bus set
+    set: bool,
+}
+
 pub struct Core {
+    /// Interrupt enable flag
+    int_enable: bool,
+    /// Default handler address
+    handler: u32,
+
+    /// Data Stack (dstack)
     dstack: Vec<u32>,
+    /// Bus interrupt states (ifile)
+    ifile: Vec<u32>,
+    /// Call Stack (cstack)
+    cstack: Vec<u32>,
+    /// Loop Stack (lstack)
+    lstack: Vec<RangeInclusive<u32>>,
 }
 
