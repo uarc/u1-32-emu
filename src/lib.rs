fc7956dc7831c22bc7eb53e1fe5a60fb2b8eafca
Geordon Worley <vadixidav@gmail.com>
2017-12-26 19:41:28 -0500

    use new uarc-emu and change internal state to match new development spec

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,62 @@
 #![feature(inclusive_range)]
 
+extern crate futures;
+extern crate num;
+
 use std::ops::RangeInclusive;
+use std::collections::BTreeMap;
+use num::{PrimInt, NumCast};
+use futures::sync::mpsc::{Sender, Receiver};
 
 #[derive(Debug, Clone, Default)]
-struct BusState {
+struct U1Bus<W> {
     /// Bus handler address
-    handler: Vec<usize>,
+    handler: W,
     /// Bus interrupt enable
     enabled: bool,
     /// Bus set
     set: bool,
+    /// If the bus is already connected
+    connected: bool,
+}
+
+#[derive(Debug, Clone)]
+struct LElem<W> {
+    program: RangeInclusive<W>,
+    index: W,
+}
+
+#[derive(Debug, Clone)]
+struct CElem<W> {
+    return_address: W,
+    dcs: [W; 2],
+    carry: bool,
+    overflow: bool,
+    int_level: W,
 }
 
 #[derive(Debug, Clone, Default)]
-pub struct Core {
+pub struct Core<W: PrimInt> {
     /// Interrupt enable flag
     int_enable: bool,
-    /// Default handler address
-    handler: usize,
     /// Program Counter
-    pc: usize,
+    pc: W,
 
+    /// Data Counters [X, Y]
+    dcs: [W; 2],
+    /// Carry bit
+    carry: bool,
+    /// Overflow bit
+    overflow: bool,
+    /// Interrupt level
+    int_level: W,
     /// Data Stack (dstack)
-    dstack: Vec<u32>,
+    dstack: Vec<W>,
     /// Bus interrupt states (ifile)
-    ifile: Vec<BusState>,
+    ifile: BTreeMap<W, U1Bus<W>>,
     /// Call Stack (cstack)
-    cstack: Vec<usize>,
+    cstack: Vec<CElem<W>>,
     /// Loop Stack (lstack)
-    lstack: Vec<RangeInclusive<usize>>,
+    lstack: Vec<LElem<W>>,
 }
 
