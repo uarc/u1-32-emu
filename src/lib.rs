9ce1fc89bd72bf2a95f5200569a50bbb4434ed7e
Geordon Worley <vadixidav@gmail.com>
2019-06-02 17:07:33 -0400

    make core just 32-bit to begin

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,33 @@
-#![feature(inclusive_range)]
-
-extern crate futures;
-extern crate num;
-
-use std::ops::RangeInclusive;
-use std::collections::BTreeMap;
-use num::{PrimInt, NumCast};
-use futures::sync::mpsc::{Sender, Receiver};
-
-#[derive(Debug, Clone, Default)]
-struct U1Bus<W> {
-    /// Bus handler address
-    handler: W,
-    /// Bus interrupt enable
-    enabled: bool,
-    /// Bus set
-    set: bool,
-    /// If the bus is already connected
-    connected: bool,
-}
-
 #[derive(Debug, Clone)]
-struct LElem<W> {
-    program: RangeInclusive<W>,
-    index: W,
-}
-
-#[derive(Debug, Clone)]
-struct CElem<W> {
-    return_address: W,
-    dcs: [W; 2],
+struct CElem {
+    return_address: u32,
+    registers: [u32; 2],
     carry: bool,
     overflow: bool,
-    int_level: W,
 }
 
 #[derive(Debug, Clone, Default)]
-pub struct Core<W: PrimInt> {
-    /// Interrupt enable flag
-    int_enable: bool,
+pub struct Core {
     /// Program Counter
-    pc: W,
+    pc: u32,
 
     /// Data Counters [X, Y]
-    dcs: [W; 2],
+    dcs: [u32; 2],
     /// Carry bit
     carry: bool,
     /// Overflow bit
     overflow: bool,
-    /// Interrupt level
-    int_level: W,
     /// Data Stack (dstack)
-    dstack: Vec<W>,
-    /// Bus interrupt states (ifile)
-    ifile: BTreeMap<W, U1Bus<W>>,
+    dstack: Vec<u32>,
     /// Call Stack (cstack)
-    cstack: Vec<CElem<W>>,
-    /// Loop Stack (lstack)
-    lstack: Vec<LElem<W>>,
+    cstack: Vec<CElem>,
+
+    /// Executed on an interrupt.
+    interrupt_handler: u32,
+    /// Determines whether an interrupt should be accepted.
+    interrupt_enable: bool,
+
+    /// Memory
+    memory: Vec<u32>,
 }
 
